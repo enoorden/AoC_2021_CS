1fb552c6c52b22617e295d7eff7d35bcb6e613f4
= <erikn@grrr.be>
2021-12-07 16:49:57 +0100

    Add project files.

---
+++ AoC_2021_CS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AoC_2021_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Day07/Day07.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day07
+{
+    class Day07
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Console.WriteLine("Day7 C#");
+
+            var fInput = File.ReadAllLines(@"..\..\..\input07.txt");
+            var input = fInput[0].Split(",").Select(int.Parse).ToList();
+
+            //Part1
+            var lowest = double.MaxValue;
+            double fuel = 0;
+            for (var i = 1; i <= input.Count; i++)
+            {
+                fuel = 0;
+                for (var j = 0; j < input.Count; j++)
+                {
+                    fuel += Math.Abs(i - input[j]);
+                }
+
+                if (fuel < lowest)
+                {
+                    lowest = fuel;
+                }
+            }
+
+            Console.WriteLine($"Part1 : {lowest}");
+
+            //Part2
+            lowest = double.MaxValue;
+            fuel = 0;
+            for (var i = 1; i <= input.Count; i++)
+            {
+                fuel = 0;
+                for (var j = 0; j < input.Count; j++)
+                {
+                    var steps = Math.Abs(i - input[j]);
+                    fuel += steps * ((steps / 2.0) + 0.5);
+                }
+
+                if (fuel < lowest)
+                {
+                    lowest = fuel;
+                }
+            }
+
+            Console.WriteLine($"Part2 : {lowest}");
+
+            stopwatch.Stop();
+            Console.WriteLine("Time elapsed: {0} ", stopwatch.Elapsed);
+        }
+    }
+}
